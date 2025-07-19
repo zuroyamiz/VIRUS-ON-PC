10bbf08dbbb849fba735bcca9777cb4add945004
👾ZUROYAMIZ👾 <computerahlong@gmail.com>
2025-07-19 20:59:17 +0700

    Add files via upload

---
+++ Test.bat
@@ -0,0 +1,19 @@
+@echo off
+:begin
+start notepad
+start wordpad
+start cmd
+start youtube
+start calc
+start clock
+start this pc
+start google chrome
+start microsoft edge
+start network
+start edge
+start photos
+start settings
+start tiktok
+start facebook
+start windows powershell
+goto begin
